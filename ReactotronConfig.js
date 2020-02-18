8082961c25acc9128defc6e6005c27192b7efeb9
Ruben Romero Diaz <r.romero@dogoodpeople.com>
2020-02-18 20:22:42 +0100

    update with prettier

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,14 +1,13 @@
 import Reactotron from 'reactotron-react-native';
-import { reactotronRedux } from 'reactotron-redux';
+import {reactotronRedux} from 'reactotron-redux';
 import sagaPlugin from 'reactotron-redux-saga';
 import AsyncStorage from '@react-native-community/async-storage';
 
-const reactotron = Reactotron
-  .setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
+const reactotron = Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
   .configure() // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
   .use(reactotronRedux())
   .use(sagaPlugin())
-  .connect()
+  .connect();
 
 export default reactotron;
